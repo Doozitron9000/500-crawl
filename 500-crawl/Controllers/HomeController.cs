54b1f16a9f84542113e55d645eefec525d35bdd2
Doozitron9000 <Doozitron9000@gmail.com>
2026-01-05 13:20:23 +1300

    Initial Commit

---
+++ 500-crawl/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _500_crawl.Models;
+
+namespace _500_crawl.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult _500Crawl()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ 500-crawl/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _500_crawl.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
