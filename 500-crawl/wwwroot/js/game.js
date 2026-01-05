9ef8cf61ce190ea2055afffe55d066fb7d765066
Doozitron9000 <Doozitron9000@gmail.com>
2026-01-06 10:50:38 +1300

    Set up basic card drawing and interpretation logic

--- 500-crawl/wwwroot/js/game.js
+++ 500-crawl/wwwroot/js/game.js
@@ -8,6 +8,27 @@ document.getElementById("drawBtn").addEventListener("click", async () => {
         return;
     }
 
-    const card = await response.json();
-    console.log(card);
+    const drawnCard = await response.json();
+
+    // get the card's value and type
+    const value = drawnCard.value;
+    const suit = drawnCard.suit;
+
+    // get the card area and make the new card
+    const cardArea = document.getElementById("cardList");
+    const newCard = document.createElement("li");
+
+    // Make arrays of suits and values to make them easier to get later
+    const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+    const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+
+    // first check if this is the joker which is the 5th suit
+    if(suit == 4){
+        newCard.textContent = "Joker";
+    } else {
+        newCard.textContent = `${values[value]} of ${suits[suit]}`;
+    }
+
+    console.log(drawnCard)
+    cardArea.append(newCard);
 })
